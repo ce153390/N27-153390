86ad6400f909437c75cc752c98dc53c6ceaccac8
ce153390 <ce153390@students.berufskolleg-borken.de>
2022-02-03 09:09:16 +0100

    ...

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
+// Programme verarbeiten oft Objekte der realen Welt.
+// Objekte haben Eigenschaften.
+// In unserem Bankingprogramm interessieren uns Objekte, 
+// wie z.B. Kunde, Konto, Filiale, Bankautomat, ...
+// Alle Kunden unserer Bank haben dieselben EIgenschaften,
+// aber unterschiedliche Eigenschaftswerte
+
+class Kunde{
+    constructor(){
+        this.IdKunde
+        this.Nachname
+        this.Vorname
+        this.Kennwort
+        this.Kontostand
+        this.Geburtsdatum
+        this.Mail
+    }
+}
+
+// Von der Kunden-Kasse wird eine konkrete Instanz gebildet
+
+let kunde=new Kunde()
+
+// Die konkrete INstanz bekommt Eigenschaftswerte zugewiesen
+
+kunde.IdKunde = 123456
+kunde.Nachname = "Genau"
+kunde.Vorname = "Ehhh"
+kunde.Geburtsdatum = "20.08.2003"
+kunde.Kennwort = "Gotcha"
+kunde.Mail = "WhatAreYouDoing@online.de"
+
+
+
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const { home } = require('nodemon/lib/utils')
 const meineApp = express()
 meineApp.set('view engine', 'ejs')
 meineApp.use(express.static('public'))
@@ -11,6 +47,20 @@ const server = meineApp.listen(process.env.PORT || 3000, () => {
 
 meineApp.get('/',(browserAnfrage, serverAntowort, next) => {              
     serverAntowort.render('index.ejs', {})          
+})
+
+meineApp.post('/login',(browserAnfrage, serverAntowort, next) => {              
+   const idKunde = browserAnfrage.body.IdKunde
+   const kennwort = browserAnfrage.body.Kennwort
+
+    console.log("ID des Kunden: " + idKunde)
+    console.log("Kennwort des Kunden: " + kennwort)
+
+    if(idKunde == Kunde.idKunde) {
+    serverAntowort.render('index.ejs', {})
+    }
+
+    serverAntowort.render('login.ejs', {})          
 })
 
 // Wenn die login-Seite im Browser aufgerufen wird, ...
