9f51c53e92c1cbf26ca8855d1e2704b022837487
ce153390 <ce153390@students.berufskolleg-borken.de>
2022-10-25 21:08:25 +0200

    Fürs Üben

--- server.js
+++ server.js
@@ -374,3 +374,4 @@ meineApp.get('/kontostandAnzeigen',(browserAnfrage, serverAntwort, next) => {
 //require('./Uebungen/ifUndElse.js')
 //require('./Uebungen/klasseUndObjekt.js')
 require('./Klausuren/20221026_klausur.js')
+
